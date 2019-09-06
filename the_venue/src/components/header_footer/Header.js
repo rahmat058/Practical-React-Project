6267c24fbde2ec9b97fb2a3a6a4315fda8c780cc
rahmat058 <md.kazi.rahmat@gmail.com>
2019-09-06 21:51:38 +0600

    Lesson 13 (Time Count Finishing is done)

--- the_venue/src/components/header_footer/Header.js
+++ the_venue/src/components/header_footer/Header.js
@@ -1,53 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/icons/Menu';
-import IconButton from '@material-ui/core/IconButton';
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import MenuIcon from "@material-ui/icons/Menu";
+import IconButton from "@material-ui/core/IconButton";
 
-import SideDrawer from './SideDrawer';
+import SideDrawer from "./SideDrawer";
 class Header extends Component {
-
   state = {
     drawerOpen: false,
     headerShow: false
-  }
+  };
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
+    window.addEventListener("scroll", this.handleScroll);
   }
 
   handleScroll = () => {
-    console.log('user scrolling');
+    console.log("user scrolling");
     if (window.scrollY > 0) {
       this.setState({
         headerShow: true
-      })
+      });
     } else {
       this.setState({
         headerShow: false
-      })
+      });
     }
-  }
+  };
 
-  toggleDrawer = (value) => {
+  toggleDrawer = value => {
     this.setState({
       drawerOpen: value
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <AppBar
         position="fixed"
-        style = {{
-          backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
-          boxShadow: 'none',
-          padding: '10px 0'
+        style={{
+          backgroundColor: this.state.headerShow ? "#2f2f2f" : "transparent",
+          boxShadow: "none",
+          padding: "10px 0"
         }}
       >
         <ToolBar>
-          <div className='header_logo'>
+          <div className="header_logo">
             <div className="font_righteous header_logo_venue">The Venue</div>
             <div className="header_logo_title">Musical Events</div>
           </div>
@@ -60,15 +59,14 @@ class Header extends Component {
             <MenuIcon />
           </IconButton>
 
-          <SideDrawer 
+          <SideDrawer
             open={this.state.drawerOpen}
-            onClose={(value) => this.toggleDrawer(value)}
+            onClose={value => this.toggleDrawer(value)}
           />
-
         </ToolBar>
       </AppBar>
-    )
+    );
   }
 }
 
-export default Header
+export default Header;
