de1bf1b6a8a3ad826ded4566b52f9b02de24224a
rahmat058 <md.kazi.rahmat@gmail.com>
2019-09-06 20:36:38 +0600

    Lesson 11 (Slider Section is done)

--- the_venue/src/App.js
+++ the_venue/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './resources/styles.css';
 
 import Header from './components/header_footer/Header';
+import Featured from './components/featured';
 
 function App() {
   return (
     <div className="App" style={{height: "1600px", background: "#74b9ff"}}>
-      <Header></Header>
+      <Header />
+      <Featured />
     </div>
   );
 }
