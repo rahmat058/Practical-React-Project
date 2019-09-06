5a6d67c41f28977a7396445f0d226b332514948a
rahmat058 <md.kazi.rahmat@gmail.com>
2019-09-06 22:40:02 +0600

    Lesson 14 (Venue NFO is done)

--- the_venue/src/App.js
+++ the_venue/src/App.js
@@ -3,12 +3,14 @@ import './resources/styles.css';
 
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
+import VenueInfo from './components/venue_info';
 
 function App() {
   return (
     <div className="App" style={{height: "1600px", background: "#74b9ff"}}>
       <Header />
       <Featured />
+      <VenueInfo />
     </div>
   );
 }
