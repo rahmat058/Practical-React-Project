1ec08dbb50dda41ebea321d8bf854bc7b2b6a862
Kazi Rahamatullah <md.kazi.rahmat@gmail.com>
2019-09-08 15:40:34 +0600

    Lesson 25 (Home featured animation is continued)

--- m_city/src/components/home/featured/Stripes.js
+++ m_city/src/components/home/featured/Stripes.js
@@ -4,18 +4,87 @@ import Animate from 'react-move/Animate';
 
 class Stripes extends Component {
 
+  state = {
+    stripes: [
+      {
+        background: '#98c5e9',
+        left: 120,
+        rotate: 25,
+        top: -260,
+        delay: 0
+      },
+      {
+        background: '#ffffff',
+        left: 360,
+        rotate: 25,
+        top: -397,
+        delay: 200
+      },
+      {
+        background: '#98c5e9',
+        left: 600,
+        rotate: 25,
+        top: -498,
+        delay: 400
+      }
+    ]
+  }
+
   showStripes = () => (
-    <div>
-      Stripes
-    </div>
+    this.state.stripes.map((stripe, i) => (
+      <Animate
+        key={i}
+        show={true}
+
+        start={{
+          background: '#ffffff',
+          opacity: 0,
+          left: 0,
+          rotate: 0,
+          top: 0
+        }}
+
+        enter={{
+          background: stripe.background,
+          opacity: [1],
+          left: stripe.left,
+          rotate: stripe.rotate,
+          top: stripe.top,
+          timing: {
+            delay: stripe.delay,
+            duration: 200,
+            ease: easePolyOut
+          },
+          events: {
+            end() {
+              console.log('Animation finished');
+            }
+          }
+        }}
+
+      >
+        {({ opacity, left, rotate, top, background }) => {
+          return (
+            <div
+              className="stripe"
+              style={{
+                background,
+                opacity,
+                transform: `rotate(${rotate}deg) translate(${left}px,${top}px)`
+              }}
+            ></div>
+          );
+        }}
+      </Animate>
+    ))
   )
 
   render() {
     return (
-      <div className="featured_stripes ">
+      <div className="featured_stripes">
         {this.showStripes()}
       </div>
-    )
+    );
   }
 }
 
