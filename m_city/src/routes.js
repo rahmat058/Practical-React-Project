e6e5ad7b8adeeed5e58775074bf5b2be274f7e04
rahmat058 <md.kazi.rahmat@gmail.com>
2019-09-10 01:09:45 +0600

    Lesson 44 (Admin matches endpoint is done)

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -9,11 +9,13 @@ import Home from './components/home';
 import SignIn from './components/signin';
 
 import Dashboard from './components/admin/Dashboard';
+import AdminMatches from './components/admin/matches';
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+        <PrivateRoutes exact {...props} component={AdminMatches} path="/admin_matches" />
         <PrivateRoutes exact {...props} component={Dashboard} path="/dashboard" />
         <PublicRoutes exact {...props} restricted={true} component={SignIn} path="/sign_in" />
         <PublicRoutes exact {...props} restricted={false} component={Home} path="/" />
