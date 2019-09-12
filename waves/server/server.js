01c65b01ca3ab10113761e6d7a327c2204fb2f10
rahmat058 <md.kazi.rahmat@gmail.com>
2019-09-12 23:22:27 +0600

    Lesson 68 (.env and middleware setup is done)

--- waves/server/server.js
+++ waves/server/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE)
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 3002;
 
