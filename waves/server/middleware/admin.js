82f4cd83caa138c302f6afa9ed921d2cbdacd276
rahmat058 <md.kazi.rahmat@gmail.com>
2019-09-14 16:04:08 +0600

    Lesson 75 (Brands model and route is done)

---
+++ waves/server/middleware/admin.js
@@ -0,0 +1,10 @@
+let admin = (req, res, next) => {
+  if(req.user.role === 0) {
+    return res.send('You are not allowed to added product, get out now!')
+  }
+  next();
+}
+
+module.exports = {
+  admin
+}
