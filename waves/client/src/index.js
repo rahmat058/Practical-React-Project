94b212ec5819c5429628420f88f02ffc63f2b82e
rahmat058 <md.kazi.rahmat@gmail.com>
2019-09-17 22:22:21 +0600

    Lesson 82 (Client setting redux setup is done)

--- waves/client/src/index.js
+++ waves/client/src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Resources/css/styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Resources/css/styles.css";
 
-import Routes from './routes';
-import { BrowserRouter } from 'react-router-dom';
+import Routes from "./routes";
+import { BrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+import rootReducer from "./store/reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
-, document.getElementById('root'));
+  <Provider
+    store={createStoreWithMiddleware(
+      rootReducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
